665a1274d3a1d0738ba6ae3abbefcb31c7b0d69c
张鹏辉 <zphui5409@sina.com>
2020-02-15 17:40:28 +0800

    add home.js

--- WorkSpace/js/navigationBar.js
+++ WorkSpace/js/navigationBar.js
@@ -1,38 +1,32 @@
-//导航栏
 Vue.component('navigation-bar', {
-  template: `
-          <nav class="navbar navbar-dark bg-dark naviHeight">
-          <a class="navbar-brand" href="/">semperhhhの博客</a>
-          <div>
-              <ul class="nav justify-content-end">
-                  <li class="nav-item naviBtn">
-                      <button class="button button-box button-longshadow-right" v-on:click="homeBtnAction()">
-                          <i class="fa fa-home"></i>
-                      </button>
-                  </li>
-                  <li class="nav-item naviBtn">
-                      <button class="button button-box button-longshadow-right" v-on:click="listsBtnAction()">
-                          <i class="fa fa-list"></i>
-                      </button>
-                  </li>
-                  <li class="nav-item naviBtn">
-                      <button class="button button-box button-longshadow-right" v-on:click="aboutBtnAction()">
-                          <i class="fa fa-user"></i>
-                      </button>
-                  </li>
-              </ul>
-          </div>
-      </nav>
-      `,
-  methods: {
-      homeBtnAction() {
-        window.location.href = "/";
-      },
-      listsBtnAction() {
-        window.location.href = "/lists";
-      },
-      aboutBtnAction() {
-        window.location.href = "/about";
-      }      
-  }
+    template: `
+    <nav class="col-md-10 navbar-center navbar navbar-bg naviHeight">
+    <a class="navbar-title">
+        <img src="images/homeImages/home-navi.png">
+    </a>
+    <div>
+        <ul class="nav justify-content-end">
+
+            <!-- 搜索 -->
+            <li class="nav-item naviBtn">
+                <div class="button navbar-btn-search">
+                    <span class="glyphicon glyphicon-search"></span>
+                    <span class="navbar-btn-search-title">搜索</span>
+                </div>
+            </li>
+
+            <!-- 返回首页 -->
+            <li class="nav-item naviBtn">
+                <button class="button navbar-btn" @click="naviBackBtnClick">
+                    <img src="images/homeImages/home-back@2x.png">
+                    <i class="navbar-backHome">返回首页</i>
+                </button>
+            </li>
+        </ul>
+    </div>
+</nav>
+    `,
+    methods: {
+
+    }
 })
